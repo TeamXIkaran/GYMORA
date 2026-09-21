85b8898bb14007c843b3da875ac88d76b7c11194
Astha Bhardwaj <asthabhardwaj529@gmail.com>
2026-09-21 20:56:29 +0530

    Add payment status endpoint

--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -4,14 +4,15 @@ import {
     submitPayment,
     approvePayment,
     rejectPayment,
+    getPaymentStatus,
 } from "../controllers/paymentController.js";
 
 const router = express.Router();
 
 router.post("/submit", submitPayment);
-
 router.post("/approve", approvePayment);
-
 router.post("/reject", rejectPayment);
+
+router.get("/status/:paymentId", getPaymentStatus);
 
 export default router;
